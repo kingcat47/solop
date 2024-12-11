d16807f07cc978d5537d48fffd4b03a05d1d6efc
orangejack <79419803+kingcat47@users.noreply.github.com>
2024-12-11 09:51:48 +0900

    zz

--- src/components/ClearBox/index.tsx
+++ src/components/ClearBox/index.tsx
@@ -11,6 +11,7 @@ export default function ClearBox({
     <>
       <div className={[styles.container, className].join(" ")}>
         <p className={styles.text}>끝.축하합니다.</p>
+        <div className={styles.bbiggabbunjjuk}></div>
         <div className={styles.close} onClick={onClick}>
           닫기
         </div>
